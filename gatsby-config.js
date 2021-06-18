bb536835eaad1f0e91113d0d2ab93ee5dd34582c
Travis Zimmerman <59665029+H2OldManZ@users.noreply.github.com>
2021-06-18 14:46:11 -0400

    Base with GraphCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: "H2O Indiana",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-graphcms',
+      options: {
+        endpoint: "https://api-us-east-1.graphcms.com/v2/ckq2dupkm0sm401xi64dl455a/master",
+      },
+    },
+
+  ],
 };
